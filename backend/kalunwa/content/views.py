a61ff9b0d094844322658f650640663d40d0663b
tremzek <thereserayemata@gmail.com>
2022-03-11 18:18:21 +0800

    feat: added image and tag models
    api works via host/api, but image link/url is currently incomplete. url lacks host and only '/image/image.jpg, instead of 'hhtps:: ... /images/image.jpg

--- backend/kalunwa/content/views.py
+++ backend/kalunwa/content/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Image
+from .serializers import ImageSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.parsers import MultiPartParser, FormParser
+
 
 # Create your views here.
+# url should be /admin
+
+# stuff here are crammed pa
+
+class ImageViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    def list(self,request):
+        queryset = Image.objects.all()
+        serializer = ImageSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Image.objects.all()
+        image = get_object_or_404(queryset, pk=pk)
+        serializer = ImageSerializer(image)
+        return Response(serializer.data)
+
+
+
+class ImageUploadView(APIView): # untested
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        print(request.data)
+        serializer = ImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else: 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/kalunwa/settings.py
+++ backend/kalunwa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # django packages
+    'rest_framework',
+
+    # user apps
+    'kalunwa.core',
+    'kalunwa.content'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# where files uplaoded using FileField go
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # plan to change api to cms/admin_panel
+MEDIA_URL = '/media/'

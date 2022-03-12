dfe63ae0aba84f2f220d9297c9305b6012697cae
Jovannie Almario <jovanniealmario@gmail.com>
2022-03-12 18:56:25 +0800

    restructured the homepage

--- frontend/kalunwa-web-app/src/app/app.component.ts
+++ frontend/kalunwa-web-app/src/app/app.component.ts
@@ -1,69 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { EventsModel } from './features/models/events';
-import { HomepageService } from './features/service/homepage.service';
-import { HomeCampModel } from './features/models/home-camp';
-import { HomeNewsModel } from './features/models/home-news';
+import { Component, ViewChild, ElementRef, ViewEncapsulation, AfterViewInit} from '@angular/core';
+import { NavService } from './features/service/nav.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit{
-  title = 'kalunwa-web-app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('sidenav') sidenav: ElementRef | undefined;
 
 
-  constructor(private homeService: HomepageService) { }
+  constructor(private navService: NavService) { }
 
 
   whatWeDo : boolean = false
   about: boolean = false;
   orgOverview : boolean = false;
 
-  public events = [] as EventsModel[];
-  public projects = [] as EventsModel[];
-  public news =[] as HomeNewsModel[];
-  public camps : HomeCampModel[] = [
-    {
-      title:"Baybayon",
-      img: "assets/images/baybayon.png",
-      color: "#ECDBB2"
-    },
-    {
-      title:"Lasang",
-      img: "assets/images/lasang.png",
-      color: "#3F6218"
-    },
-    {
-      title:"Suba",
-      img: "assets/images/suba.png",
-      color: "#1C8BD4"
-    },
-    {
-      title:"Zero Waste",
-      img: "assets/images/zero.png",
-      color: "#9CCC65"
-    },
-  ];
-
-  ngOnInit(): void {
-    this.getEventsDisplay();
-    this.getProjectsDisplay();
-    this.getNewsDisplay();
+  ngAfterViewInit() {
+    this.navService.sidenav = this.sidenav;
   }
 
-  getEventsDisplay(): void {
-    this.events = this.homeService.getEvents();
-    console.log(this.events);
-  }
-
-  getProjectsDisplay(): void {
-    this.projects = this.homeService.getProjects();
-    console.log(this.projects);
-  }
-
-  getNewsDisplay(): void {
-    this.news = this.homeService.getNews();
-    console.log(this.news);
-  }
 }
--- frontend/kalunwa-web-app/src/app/features/components/nav-bar/nav-bar.component.ts
+++ frontend/kalunwa-web-app/src/app/features/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavService } from '../../service/nav.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public navService : NavService) { }
   whatWeDo : boolean = false
   about: boolean = false;
   orgOverview : boolean = false;

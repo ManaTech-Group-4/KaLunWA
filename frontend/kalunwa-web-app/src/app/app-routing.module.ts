da4224d84ccf187fa0c16a1cf77db28ce92cef30
Jovannie Almario <jovanniealmario@gmail.com>
2022-03-16 17:35:03 +0800

    about us template & page not found template route

--- frontend/kalunwa-web-app/src/app/app-routing.module.ts
+++ frontend/kalunwa-web-app/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './features/components/about-page/about-page.component';
+import { HomepageComponent } from './features/components/homepage/homepage.component';
+import { PageNotFoundComponent } from './features/components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomepageComponent},
+  {path: 'about',  component: AboutPageComponent},
+  {path: "**", component: PageNotFoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routeComponents = [HomepageComponent,
+                                AboutPageComponent,
+                                PageNotFoundComponent];
--- frontend/kalunwa-web-app/src/app/app.module.ts
+++ frontend/kalunwa-web-app/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatDesignModule } from './features/shared/mat-design.module';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routeComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './features/components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JumbotronComponent } from './features/components/jumbotron/jumbotron.component';
 import { MatCarouselModule } from '@ngmodule/material-carousel';
-import { HomepageComponent } from './features/components/homepage/homepage.component';
 import { FooterComponent } from './features/components/footer/footer.component';
 
 @NgModule({
@@ -15,8 +14,8 @@ import { FooterComponent } from './features/components/footer/footer.component';
     AppComponent,
     NavBarComponent,
     JumbotronComponent,
-    HomepageComponent,
-    FooterComponent
+    FooterComponent,
+    routeComponents
   ],
   imports: [
     BrowserModule,

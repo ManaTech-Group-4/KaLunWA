6d141995beaef583d19281319216039189ec54b3
tremzek <thereserayemata@gmail.com>
2022-03-23 16:52:19 +0800

    feat: added development tools (auto-populate & test_file deletion scripts)

---
+++ backend/dev_utils/delete_test_files.py
@@ -0,0 +1,11 @@
+import glob
+import os
+
+# deletes everything in the directory (non-recursive) that starts with 'test'
+file_list = glob.glob('media/images/content/test*')
+for file in file_list:
+    os.remove(file)
+
+
+# exec(open("dev_utils/delete_test_files.py").read())
+

ebef09841047b0e6a095b61c33d1ae18d47ce4ff
Jovannie Almario <jovanniealmario@gmail.com>
2022-03-31 15:02:41 +0800

    additional testing for dependency injections and routing

--- frontend/kalunwa-web-app/src/app/features/components/about-page/about-page.component.spec.ts
+++ frontend/kalunwa-web-app/src/app/features/components/about-page/about-page.component.spec.ts
@@ -1,33 +1,39 @@
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule,} from '@angular/common/http/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { AboutpageService } from '../../service/aboutpage.service';
 
 import { AboutPageComponent } from './about-page.component';
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent;
   let fixture: ComponentFixture<AboutPageComponent>;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  let testBedService : AboutpageService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AboutPageComponent ],
-      imports : [ HttpClientTestingModule ]
+      imports : [ HttpClientTestingModule ],
+      providers: [AboutpageService]
 
     })
     .compileComponents();
-    httpClient = TestBed.get(HttpClient);
-    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    testBedService = TestBed.get(AboutpageService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('Service injected via inject() and TestBed.get() should be the same (AboutpageService)',
+    inject([AboutpageService], (injectService: AboutpageService) => {
+      expect(injectService).toBe(testBedService);
+  }));
 });

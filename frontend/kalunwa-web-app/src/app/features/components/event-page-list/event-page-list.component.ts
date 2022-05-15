5bdc1f8a728b4071895509727ca69ad657787e45
Jovannie Almario <jovanniealmario@gmail.com>
2022-05-15 17:14:43 +0800

    pagination event

--- frontend/kalunwa-web-app/src/app/features/components/event-page-list/event-page-list.component.ts
+++ frontend/kalunwa-web-app/src/app/features/components/event-page-list/event-page-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { EventsItemsModel } from '../../models/event-items-model';
 import { EventspageService } from '../../service/eventspage.service';
 
@@ -11,9 +12,40 @@ export class EventPageListComponent implements OnInit {
 
   @Input()
   events = [] as EventsItemsModel[];
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) { }
 
+  activePage:number = 1;
+  currentPage = 0;
+  lastPage = 4;
 
+  detectIfChanges(){
+    this.ref.detectChanges();
+  }
+
+  updateDisplay(newPage:number){
+    if(newPage > this.activePage){
+      this.currentPage += 5;
+      if(this.lastPage+5 > this.events.length){
+        this.lastPage = this.events.length;
+        return;
+      }
+      else
+        this.lastPage += 5;
+    }
+    else if(newPage < this.activePage){
+      this.lastPage -= 5;
+      if(this.currentPage-5 <0){
+        this.currentPage = this.events.length;
+        return;
+      }
+      else
+        this.currentPage -= 5;
+    }
+    this.ref.detectChanges();
+    this.activePage = newPage;
+    let y =  document.querySelector('.event-card')?.getBoundingClientRect().top;
+    window.scrollTo({top: y! + window.scrollY - 80, behavior: 'smooth'});
+  }
 
 
   ngOnInit(): void {

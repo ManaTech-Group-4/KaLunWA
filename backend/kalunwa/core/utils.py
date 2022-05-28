9c01146efe99cb5ff0e461ef91603309c4a1bb71
tremzek <thereserayemata@gmail.com>
2022-05-29 06:22:31 +0800

    feat: unique slugify function

--- backend/kalunwa/core/utils.py
+++ backend/kalunwa/core/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from django.utils.text import slugify
 
 def to_formal_mdy(date:datetime)->str:
         if not date:
@@ -14,3 +14,13 @@ def get_value_by_label(label:str, Enum):
 		if label == enum_obj.label: 
 			value = enum_obj.value
 	return value
+
+def unique_slugify(instance, string):
+	model = instance.__class__
+	slug = slugify(string)
+	unique_slug = slug
+	suffix = 1
+	while model.objects.filter(slug=unique_slug).exists():
+		unique_slug = f'{slug}-{suffix}'
+		suffix += 1
+	return unique_slug 

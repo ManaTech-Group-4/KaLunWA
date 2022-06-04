62bce26f97511f3456ff3ad460d36233c9a7b582
tremzek <thereserayemata@gmail.com>
2022-06-04 23:00:48 +0800

    fix: corrected permissions that allowed a normal user to delete another

--- backend/kalunwa/users/permissions.py
+++ backend/kalunwa/users/permissions.py
@@ -3,96 +3,72 @@ from rest_framework.permissions import (
     SAFE_METHODS, # http methods -> get, head, options
 )
 
-class AdminAuthorPermission(BasePermission):
+
+class DefaultObjectPermission(BasePermission):
     """
-    methods should return a boolean value (true or false)
-    """
-    message = 'Adding customers not allowed.'
+    To watch out for when using bit OR in views:
+        https://github.com/encode/django-rest-framework/issues/7117
 
-    def has_permission(self, request, obj, view):
-        """
-        if is_authenticated and
-        is_admin         # 
-        | is_super_admin # add and delete 
-        | is_super_user  # can do anything
-        """
-        pass
+    override default has_object_permission from:
+            return True
+        to ->
+            return has_permission(...)
 
-
-    def has_object_permission(self, request, view, obj):
-        pass
+    """    
+    def has_object_permission(self, request, view, obj): # pass -> false
+        return self.has_permission(request, view)    
 
 
-class SuperAdminAuthorPermission(BasePermission):
-    pass
-
-
-class SuperUserOnly(BasePermission):
+class SuperUserOnly(DefaultObjectPermission):
     """
     Superusers are the only one allowed to access the views with this permission.
+    Must be superuser as well to be granted object permissions. 
     """
     def has_permission(self, request, view):
         """
-        only superusers can access list. 
-        """
-        print(request.user.is_superuser)
-        if not request.user.is_authenticated:
-            return False        
+        only superusers can access the endpoint. 
+        """           
         return request.user.is_superuser
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_superuser
-        
 
-class AuthenticatedOnlyWithSafeMethods(BasePermission):
+class AuthenticatedAndReadOnly(DefaultObjectPermission):
     """
-    Only Authenticated users are allowed on marked detail views, though they have limitations.
+    Only Authenticated users are allowed on endpoints, though they have limitations.
     They can only use safe methods: 
         GET, OPTIONS, HEAD
     """    
-    def has_permission(self, request, view):
+    def has_permission(self, request, view): 
         if request.user.is_authenticated and request.method in SAFE_METHODS:
             return True
         return False
 
-
-class AccountOwnerOnlyCanEditDeleteDetail(BasePermission):
+class SelfUserOnly(DefaultObjectPermission):
     """
     Only respective users can edit their user/account info.
     Note:
         - This is different to ownerCanEditDeleteOnly, as the latter refers
         to views wherein the objects/records involved are being owned by the user. 
     """
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):                      
-        # authenticated users can look at user basic info (email etc.)
-        if request.method in SAFE_METHODS:
-            return True
+    def has_object_permission(self, request, view, obj):                              
         # Owners and superusers can update and delete themselves            
         if request.user.id == obj.id:
             return True
-
         return False      
-class OwnersOnlyCanEditDelete(BasePermission):
+
+        
+class OwnersOnly(DefaultObjectPermission):
     """
     Only user owners of a record can edit/delete what ever they owned. 
     """
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
-        # authenticated users can look at user basic info (email etc.)
-        if request.method in SAFE_METHODS:
-            return True
         # Owners and superusers can update and delete themselves            
         if request.user == obj.user:
             return True
-
         return False    
   
 
+
+########################################## not used 
 class IsProfileOwnerOrReadOnlyAdmin(BasePermission):
     """
     - User owning the profile can read and edit
@@ -132,7 +108,28 @@ class IsProfileOwnerOrReadOnlyAdmin(BasePermission):
         return obj.owner == request.user 
 
 
+class AdminAuthorPermission(BasePermission):
+    """
+    methods should return a boolean value (true or false)
+    """
+    message = 'Adding customers not allowed.'
 
+    def has_permission(self, request, obj, view):
+        """
+        if is_authenticated and
+        is_admin         # 
+        | is_super_admin # add and delete 
+        | is_super_user  # can do anything
+        """
+        pass
+
+
+    def has_object_permission(self, request, view, obj):
+        pass
+
+
+class SuperAdminAuthorPermission(BasePermission):
+    pass
 
 # check the roles before doing an action?
 # using these because we can attach them to classes .. and are much cleaner

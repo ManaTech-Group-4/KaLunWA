5e0a6331237b0248ec371a304c6ee3123c13dea7
eacasquejo <86784957+eacasquejo@users.noreply.github.com>
2022-06-05 15:02:54 +0800

    updated flex display for admin-list

--- frontend/kalunwa-web-app/src/app/features/components/CMS/administrators/admin-list/admin-list.component.ts
+++ frontend/kalunwa-web-app/src/app/features/components/CMS/administrators/admin-list/admin-list.component.ts
@@ -39,6 +39,36 @@ export class AdminListComponent implements OnInit {
       role: "Admin",
       date_added: "09/12/21",
     },
+    {
+      id:1,
+      picture: "assets/images/person-icon.jpg",
+      username: "admin_1",
+      first_name: "Jose",
+      last_name: "Rizal",
+      email: "j.riz@gmail.com",
+      role: "Super Admin",
+      date_added: "12/30/21",
+    },
+    {
+      id:2,
+      picture: "assets/images/person-icon.jpg",
+      username: "admin_2",
+      first_name: "Juan",
+      last_name: "Dela Cruz",
+      email: "jdcruz@gmail.com",
+      role: "Admin",
+      date_added: "04/05/21",
+    },
+    {
+      id:3,
+      picture: "assets/images/person-icon.jpg",
+      username: "admin_3",
+      first_name: "Joferlyn",
+      last_name: "Robs",
+      email: "doc.joferlyn@gmail.com",
+      role: "Admin",
+      date_added: "09/12/21",
+    },
   ]
   
   selectedAdmin?:AdminListModel = this.admin_list[0];

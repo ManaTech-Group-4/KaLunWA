258ad147e9fcf35e1c57c61419deb2e66bb76e9c
Jovannie Almario <jovanniealmario@gmail.com>
2022-06-07 21:53:19 +0800

    homepage display http fix

--- frontend/kalunwa-web-app/src/app/features/service/homepage.service.ts
+++ frontend/kalunwa-web-app/src/app/features/service/homepage.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EventsModel } from '../models/events';
 import { HomeNewsModel } from '../models/home-news';
+import { homepageInfo } from '../models/homepage-container-model';
 import { JumbotronModel } from '../models/slides-model';
 
 
@@ -28,8 +29,8 @@ export class HomepageService {
     return this.http.get<HomeNewsModel[]>('http://127.0.0.1:8000/api/news/?expand=image&omit=created_at,updated_at,image.id&query_limit=3');
   }
 
-  public getJumbotron():Observable<JumbotronModel[]> {
-    return this.http.get<JumbotronModel[]>('http://127.0.0.1:8000/api/jumbotrons/?expand=image&omit=created_at,updated_at,image.id&query_limit=5');
+  public getHomepage():Observable<homepageInfo.HomepageContainer> {
+    return this.http.get<homepageInfo.HomepageContainer>('http://127.0.0.1:8000/api/page-containers/homepage/?expand=page_contained_jumbotrons.jumbotron.image,page_contained_events.event.image,page_contained_projects.project.image');
   }
 
 }

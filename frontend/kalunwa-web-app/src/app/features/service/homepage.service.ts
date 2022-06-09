27a19169c3f43f9af561bb846762b1d9492880eb
Jovannie Almario <jovanniealmario@gmail.com>
2022-06-09 14:07:55 +0800

    test fixes

--- frontend/kalunwa-web-app/src/app/features/service/homepage.service.ts
+++ frontend/kalunwa-web-app/src/app/features/service/homepage.service.ts
@@ -17,13 +17,13 @@ export class HomepageService {
   constructor(private http:HttpClient) { }
 
 
-  public getEvents(): Observable<EventsModel[]>{
-    return this.http.get<EventsModel[]>('http://127.0.0.1:8000/api/events/?expand=image&fields=id,title,image.image&is_featured=True&query_limit=3');
-  }
+  // public getEvents(): Observable<EventsModel[]>{
+  //   return this.http.get<EventsModel[]>('http://127.0.0.1:8000/api/events/?expand=image&fields=id,title,image.image&is_featured=True&query_limit=3');
+  // }
 
-  public getProjects(): Observable<EventsModel[]> {
-    return this.http.get<EventsModel[]>('http://127.0.0.1:8000/api/projects/?expand=image&fields=id,title,image.image&is_featured=True&query_limit=3');
-  }
+  // public getProjects(): Observable<EventsModel[]> {
+  //   return this.http.get<EventsModel[]>('http://127.0.0.1:8000/api/projects/?expand=image&fields=id,title,image.image&is_featured=True&query_limit=3');
+  // }
 
   public getNews(): Observable<HomeNewsModel[]> {
     return this.http.get<HomeNewsModel[]>('http://127.0.0.1:8000/api/news/?expand=image&omit=created_at,updated_at,image.id&query_limit=3');

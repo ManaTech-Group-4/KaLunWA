c731d8081cedae8fda15dddea7a2213895ea37c6
tremzek <thereserayemata@gmail.com>
2022-06-11 19:30:11 +0800

    settings: remove session auth

--- backend/kalunwa/settings.py
+++ backend/kalunwa/settings.py
@@ -158,15 +158,9 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',     
     ),
     'EXCEPTION_HANDLER':'kalunwa.core.exceptions.custom_exception_handler',
-
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
-
 }
 
 SIMPLE_JWT = {
